c6b6daa7cb53fe60b508b6738628f84333f58a3b
Julian Pollinger <julian.pollinger@jp-studios.de>
2021-05-12 11:09:45 +0200

    build(semantic-release): added

---
+++ .releaserc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    release: {
+        branches: ['master', {"name": "beta", "prerelease": true}]
+    },
+    "plugins": [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        ["@semantic-release/github", {
+            "assets": ["build/linux/linpackParser_linux", "build/macos/linpackParser_mac", "build/windows/linpackParser_win.exe"]
+        }],
+        ["@semantic-release/git", {
+            "assets": ["package.json"],
+            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+        }],
+        "@semantic-release/npm"
+    ],
+    "preset": "angular"
+}
