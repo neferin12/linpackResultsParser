79de5a079fefbd30bdac1bb2f7296376bd34de77
Julian Pollinger <julian.pollinger@jp-studios.de>
2021-05-12 11:00:22 +0200

    fix: benanntes csv file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 
 const fs = require('fs');
+const path = require('path');
 const term = require('terminal-kit').terminal;
 const ObjectsToCsv = require('objects-to-csv');
 
@@ -8,7 +9,7 @@ try {
     (async () => {
         term('Choose a file: ');
         const input = await term.fileInput();
-        term.green("\nParsing '%s'\n", input);
+        await term.green("\nParsing '%s'\n", input);
 
         const x = await term.singleColumnMenu(['N', 'NB', 'P', 'Q', 'Time']).promise
         fs.readFile(input, "utf8", function (err, data) {
@@ -21,8 +22,12 @@ try {
                 return ret;
             });
 
+            const resultFile = `./${path.basename(input)}.csv`
+
             const csv = new ObjectsToCsv(reses)
-            csv.toDisk('./results.csv').finally(() => process.exit(0));
+            csv.toDisk(resultFile, {}).finally(() => process.exit(0));
+
+            term.green("\nSaved %d results to '%s'\n",reses.length, resultFile);
         });
     })();
 } catch (e){
